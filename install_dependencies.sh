a61c4514a1d21a7e793c0628883cbc8f7d950a31
brepnm <96025456+brepnm@users.noreply.github.com>
2025-12-29 16:23:54 +0300

    update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,14 +3,4 @@ sudo apt install neovim
 sudo apt install eza
 sudo apt install bat
 
-echo -e "{}" > ~/.shortcuts.json
-# fill the json with default values
-$default_shortcuts='{
-    "default": "/",
-    "var": "/var",
-    "etc": "/etc",
-    "nginx": "/etc/nginx",
-    "www": "/var/www"
-}'
-
-echo -e $default_shortcuts > ~/.shortcuts.json
+echo -e '{"default": "/","var": "/var","etc": "/etc","nginx": "/etc/nginx","www": "/var/www"}' > ~/.shortcuts.json
