3be949d3c645d09aad84634d65eaa535f62dab45
brepnm <96025456+brepnm@users.noreply.github.com>
2025-12-29 16:19:43 +0300

    update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,3 +4,13 @@ sudo apt install eza
 sudo apt install bat
 
 echo -e "{}" > ~/.shortcuts.json
+# fill the json with default values
+$default_shortcuts='{
+    "default": "/",
+    "var": "/var",
+    "etc": "/etc",
+    "nginx": "/etc/nginx",
+    "www": "/var/www"
+}'
+
+echo -e $default_shortcuts > ~/.shortcuts.json
